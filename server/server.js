e9d9637d91314be42b8a642221a4aadb761d2b13
prashant075 <guptaprashant075.com>
2018-06-03 20:41:35 +0530

    Add GET/id Route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo}= require('./models/todo');
@@ -31,6 +32,30 @@ app.get('/todos',(req,res)=>{
   });
 });
 
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(400).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+//   {
+//     "todo": {
+//         "completed": false,
+//         "_id": "5b13960c4730b43f80cef6af",
+//         "text": "First",
+//         "__v": 0
+//     }
+// }
+
+//  res.send(req.params);
+});
  app.listen(3000,()=>{
    console.log('Started on port 3000');
  });
